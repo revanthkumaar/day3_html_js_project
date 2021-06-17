a92d4100b12efac2010505edab8ea4ababe161be
revanthkumaar <iamrevanthkumar@gmail.com>
2021-06-17 20:57:43 +0530

    Initial commit

---
+++ gallery.js
@@ -0,0 +1,18 @@
+function firstFunction(){
+    const a = 10
+    console.log(a)
+    console.log('first button clicked');
+    alert('checking alert')
+
+}
+
+function secondFunction(){
+    let a = 80
+    a=100
+    console.log(a)
+    console.log('second button clicked')
+}
+
+function thirdFunction(){
+    console.log('third button clicked')
+}
